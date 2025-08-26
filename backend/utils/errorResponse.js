d936976933fc7f2eb4711d6edb96c4e6c1ba3688
hemanthkolli22 <hemanthsrkit@gmail.com>
2025-08-26 10:23:30 +0530

    Delete backend directory

--- backend/utils/errorResponse.js
+++
@@ -1,6 +0,0 @@
-// utils/errorResponse.js
-const errorResponse = (res, message, statusCode = 400) => {
-  return res.status(statusCode).json({ success: false, message });
-};
-
-export default errorResponse;
--- backend/utils/successResponse.js
+++
@@ -1,6 +0,0 @@
-// utils/successResponse.js
-const successResponse = (res, data, statusCode = 200) => {
-  return res.status(statusCode).json({ success: true, ...data });
-};
-
-export default successResponse;
