e2d3655d13542fc25bcb32184a1f6de34d30e428
hemanthkolli22 <hemanthsrkit@gmail.com>
2025-08-26 10:31:49 +0530

    Update API's.js

--- frontend/src/API's.js
+++ frontend/src/API's.js
@@ -1,2 +1,7 @@
-export const registerAPI = "http://localhost:5000/api/auth/register";
-export const loginAPI = "http://localhost:5000/api/auth/login";
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5000"
+    : "https://backend-render-1-279z.onrender.com";
+
+export const registerAPI = `${BASE_URL}/api/auth/register`;
+export const loginAPI = `${BASE_URL}/api/auth/login`;
